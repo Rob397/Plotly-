6ad536662bc01082711b4b8a0a871f25f17a4c4d
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-08-21 11:54:44 +1000

    added code

--- static/app.js
+++ static/app.js
@@ -1,4 +1,5 @@
 // Use the D3 library to read in samples.json.
+
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
 // Use sample_values as the values for the bar chart.
@@ -6,6 +7,75 @@
 // Use otu_ids as the labels for the bar chart.
 
 // Use otu_labels as the hovertext for the chart.
+
+
+function BuildDATA(sample) {
+    // @TODO: Complete the following function that builds the metadata panel
+    // Use `d3.json` to fetch the metadata for a sample
+    var url = "../samples.json";
+
+    // C:\Users\mr-ma\Documents\GitHub\Plotly-\samples.json
+    
+    d3.json(url).then((sample) => {
+  
+      // Use d3 to select the panel 
+      var sample_metadata = d3.select("#sample-metadata");
+  
+      // clear any existing metadata
+      sample_metadata.html("");
+  
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata.
+      Object.entries(sample).forEach(function ([key, value]) {
+        var row = sample_metadata.append("panel-body");
+        row.text(`${key}: ${value} \n`);
+      });
+    });
+  }
+
+  
+
+
+
+  function buildCharts(sample) {
+
+    // @TODO: Use `d3.json` to fetch the sample data for the plots
+    var url = "../samples.json";
+    d3.json(url).then(function(data) {
+  
+      // @TODO: Bar Chart using the sample data
+      var x_values = data.otu_ids;
+      var y_values = data.sample_values;
+      var m_size = data.sample_values;
+      var m_colors = data.otu_ids; 
+      var t_values = data.otu_labels;
+  
+      var trace1 = {
+        x: x_values,
+        y: y_values,
+        text: t_values,
+        type: 'bar',
+        marker: {
+          color: m_colors,
+          size: m_size
+        }, 
+        orientation:'h',
+      };
+      
+      var layout = {
+        title: "top 10 OTUs",
+        barmode: "group"
+      };
+
+
+      var traceData = [trace1];
+      Plotly.newPlot('bar', traceData, layout);
+
+    
+  })
+}
+
+buildCharts();
 
 
 
