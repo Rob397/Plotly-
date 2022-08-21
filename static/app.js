d96875ef177ebfb10dcfb152b204bdf7c7c61d73
Rob397 <105466612+Rob397@users.noreply.github.com>
2022-08-21 16:22:19 +1000

    changed a few things

--- static/app.js
+++ static/app.js
@@ -10,89 +10,102 @@
 
 
 function BuildDATA(sample) {
-    // @TODO: Complete the following function that builds the metadata panel
+   
     // Use `d3.json` to fetch the metadata for a sample
     var url = "../samples.json";
 
     // C:\Users\mr-ma\Documents\GitHub\Plotly-\samples.json
     
-    d3.json(url).then((sample) => {
+    d3.json(url).then((data) => {
   
+      let metadata = data.metadata;
+    
+      // let id = metadata.id;
+      // let ethnicity = metadata.ethnicity;
+      // let gender =metadata.gender;
+      // let age = metadata.age;
+      // let location =metadata.location;
+      // let bbtype = metadata.bbtype;
+      // let wfreq = metadata.wfreq;
+
+      let resultArray = metadata.filter(object => object.id == data);
+      let result = resultArray[0]
+      console.log(result)
+
       // Use d3 to select the panel 
-      var sample_metadata = d3.select("#sample-metadata");
+      var metaPanel = d3.select("#sample-metadata");
   
       // clear any existing metadata
-      sample_metadata.html("");
+      metaPanel.html("");
   
       // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new tags for each key-value in the metadata.
-      Object.entries(sample).forEach(function ([key, value]) {
-        var row = sample_metadata.append("panel-body");
+      // Inside the loop, you will need to use d3 to 
+      // append new tags for each key-value in the metadata.
+      Object.entries(result).forEach(function ([key, value]) {
+        var row = metaPanel.append("panel-body");
         row.text(`${key}: ${value} \n`);
       });
     });
   }
 
+
+// #need to naviate into the json file before doing anything
   
 
 
 
-  function buildCharts(sample) {
+//   function buildCharts(sample) {
 
-    // @TODO: Use `d3.json` to fetch the sample data for the plots
-    var url = "../samples.json";
-    d3.json(url).then(function(data) {
+//     // `d3.json` to fetch the sample data for the plots
+//     var url = "../samples.json";
+//     d3.json(url).then(function(data) {
   
-      // @TODO: Bar Chart using the sample data
-      var x_values = data.otu_ids;
-      var y_values = data.sample_values;
-      var m_size = data.sample_values;
-      var m_colors = data.otu_ids; 
-      var t_values = data.otu_labels;
+//       // Bar Chart using the sample data
+//       var x_values = data.otu_ids;
+//       var y_values = data.sample_values;
+//       var m_size = data.sample_values;
+//       var m_colors = data.otu_ids; 
+//       var t_values = data.otu_labels;
   
-      var trace1 = {
-        x: x_values,
-        y: y_values,
-        text: t_values,
-        type: 'bar',
-        marker: {
-          color: m_colors,
-          size: m_size
-        }, 
-        orientation:'h',
-      };
+//       var trace1 = {
+//         x: x_values,
+//         y: y_values,
+//         text: t_values,
+//         type: 'bar',
+//         marker: {
+//           color: m_colors,
+//           size: m_size
+//         }, 
+//         orientation:'h',
+//       };
       
-      var layout = {
-        title: "top 10 OTUs",
-        barmode: "group"
-      };
+//       var layout = {
+//         title: "top 10 OTUs",
+//         barmode: "group"
+//       };
 
 
-      var traceData = [trace1];
-      Plotly.newPlot('bar', traceData, layout);
+//       var traceData = [trace1];
+//       Plotly.newPlot('bar', traceData, layout);
 
     
-  })
-}
-
-buildCharts();
+//   })
+// }
 
 
 
 
 
+// let samples = data.samples;
+// let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+// let result = resultArray[0];
 
+// let otu_ids = result.otu_ids;
+// let otu_labels = result.otu_labels;
+// let sample_values = result.sample_values;
 
-
-
-
-
-
-
-
-
-
-
+// 12:23
+// let yticks = otu_ids.slice(0, 10).map(otuID => OTU ${otuID}).reverse()
 
 
 
